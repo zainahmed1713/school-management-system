9a3add7a8aac0c48f4db5f63da38de2c8d7ab180
Zain Ahmed <zainahmed1713@gmail.com>
2021-06-27 16:02:03 +0500

    Updated the whole program
    Added a main menu from where the user can navigate and fixed minor bugs.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,82 @@
+// This is a group project submission for Computer Programming Lab.
+
+// Project Name: School Management System
+// Class: BS-IT 1A
+// Team Members: Salman Abid, Ahsan, Zain
 #include <iostream>
+#include <conio.h>
 #include <fstream>
 using namespace std;
 
+
+// Function Prototypes
+void main_menu();
 void student_data();
 void new_student();
 void fee_calculator();
 
+// Structure to store New Student's Details
+struct student_details
+{
+    string first_name, last_name, enrollment;
+};
+
 int main()
 {
-    
+    int choice;
+    while(choice != 4)
+    {
+        cout << "\t\t\tWelcome to Bahria University IT Department\n";
+        cout << "\nMenu:\n\n";
+        cout << "1. Show Records\n2. Add a New Student\n3. Fee Calculator\n4. Exit\n";
+        cout << "Enter your choice\n";
+        cin >> choice;
+        
+        switch(choice)
+        {
+            case 1:
+            {
+                student_data();
+                
+                break;
+            }
 
+            case 2:
+            {
+                new_student();
 
+                break;
+            }
+
+            case 3:
+            {
+                fee_calculator();
+
+                break;
+            }
+
+            case 4:
+            {
+                break;
+            }
+
+            default:
+            {
+                cout << "Invalid Input\n";
+
+                break;
+            }
+        }
+    }
 
     system("pause");
     return 0;
 }
 
+// Function Declaration
 void fee_calculator()
 {
+    system("cls");
     int fee_per_credit_hour = 4200;
     int credit_hour, total_fee;
     
@@ -27,41 +86,52 @@ void fee_calculator()
     total_fee = fee_per_credit_hour * credit_hour;
 
     cout << "Your fee for this semester would be = " << total_fee << "\n";
+    
+    cout << "Press any key to jump to main menu\n";
+    getch();
+    system("cls");
 }
 
 void new_student()
 {
-    
-    fstream writeData("studentdata.txt", ios::in | ios::out | ios::app);
-    string enrollment;
+    system("cls");
+    char add_another;
+
+    // Details is structure variable
+    student_details details;
+    ofstream writeData("studentdata.txt", ios::in | ios::out | ios::app);
     char choice;
-    char name[30];
-    
-    while(choice != 'n' && choice != 'N')
+
+    // Asking for New Student's Details and appending it to the file
+    while(add_another != 'n' && add_another != 'N')
     {
-        system("cls");
-    cout << "Enter new student's name\n";
-    
-    cin.getline(name, 30);
+    cout << "Enter Student's First Name\n";
+    cin >> details.first_name;
+    cout << "Enter Student's Last Name\n";
+    cin >> details.last_name;
+    cout << "Assign an enrollment id\n";
+    cin >> details.enrollment;
 
-    cout << "Assign enrollment id for " << name << "\n";
-    cin >> enrollment;
-    writeData << "\n" << enrollment << "           |       " << name;
-
+    writeData << "\n" << details.enrollment << "           |       " << details.first_name << " " << details.last_name;
     cout << "Do you want to add another student?(y/n)\n";
-    cin >> choice;
-
+    cin >> add_another;
     cin.ignore();
     }
     writeData.close();
+
+    cout << "Press any key to jump to main menu\n";
+    getch();
+    system("cls");
 
 }
 
 void student_data()
 {
+    system("cls");
     string studentData;
     ifstream read_student_data("studentdata.txt");
-
+    
+    // Displaying Data line by line
     while(getline(read_student_data, studentData))
     {
         cout << studentData << endl;
@@ -69,5 +139,9 @@ void student_data()
     }
 
     read_student_data.close();
+
+    cout << "Press any key to jump to main menu\n";
+    getch();
+    system("cls");
 
 }
